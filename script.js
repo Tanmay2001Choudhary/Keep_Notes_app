7b1b314bd5a3967dcf530c232939bbfa5475269f
Tanmay Choudhary <tanmay2001choudhary@gmail.com>
2022-06-25 20:00:58 +0530

    Tanmay keep notes uploaded

---
+++ script.js
@@ -0,0 +1,63 @@
+const addButton = document.querySelector('#add')
+
+const updateLocalStorageData = () => {
+  const textAreaData = document.querySelectorAll('textarea')
+  const notes = []
+  console.log(textAreaData)
+  textAreaData.forEach((note) => {
+    return notes.push(note.value)
+  })
+  console.log(notes)
+  localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+const addNewNote = (text = '') => {
+  const note = document.createElement('div')
+  note.classList.add('note') // it will create div with class name note
+
+  const htmlData = `
+  <div class="operation">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+      </div>
+      <textarea class="${text ? 'hidden' : ''}"></textarea>
+      <div class="main ${text ? 'hidden' : ''}"></div>
+`
+  note.insertAdjacentHTML('afterbegin', htmlData)
+
+  // getting the reference
+  const editButton = note.querySelector('.edit')
+  const delButton = note.querySelector('.delete')
+  const mainDiv = note.querySelector('.main')
+  const textArea = note.querySelector('textarea')
+
+  // Deleting the node
+  delButton.addEventListener('click', () => {
+    note.remove()
+    updateLocalStorageData()
+  })
+
+  // Toggle using edit buttton
+  textArea.value = text
+  mainDiv.innerHTML = text
+  editButton.addEventListener('click', () => {
+    mainDiv.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+  })
+
+  textArea.addEventListener('change', (event) => {
+    const value = event.target.value
+    mainDiv.innerHTML = value
+    updateLocalStorageData()
+  })
+
+  document.body.appendChild(note)
+}
+// Getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note))
+}
+
+addButton.addEventListener('click', () => addNewNote())
